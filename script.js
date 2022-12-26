803d8590e915b34b2d3af8e8441d191c4dde75ef
o1-spec <oluwafemionadokun@gmail.com>
2022-12-26 12:31:18 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//Implementing date in footer
+let date = document.querySelector('.date')
+const year = new Date().getFullYear()
+date.textContent = year
+
+// Implementing collection scroll
+
+const collection = [...document.querySelectorAll('.collection-boxes')];
+const nextBtn = [...document.querySelectorAll('.btn-2-collection')];
+const prevBtn = [...document.querySelectorAll('.btn-1-collection')]
+
+collection.forEach(function(item,i){
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nextBtn[i].addEventListener('click',function(){
+        item.scrollLeft +=  containerWidth;
+    })
+
+    prevBtn[i].addEventListener('click',function(){
+        item.scrollLeft -= containerWidth;
+    })
+})
